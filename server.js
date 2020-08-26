f197720be51267fd8c7313db825b22e9028a65e0
brianbickes <brianbickes@gmail.com>
2020-08-26 12:45:43 -0700

    add server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express(); 
 
+const mongoose = require('mongoose');
+const mongoURI = "mongodb+srv://brianbickes:SableDawg3!@sei.w6o9a.azure.mongodb.net/fruits-sei?"
 
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo');
+});
+
+const Fruit = require("./models/fruits.js")
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: true })); //makes req.body functional 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 
 app.listen(3000, () => {
